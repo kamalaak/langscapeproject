73a4d2d1e4b99d7f76e524799025daf890a1cb23
kamalaak <kamalstiries007@gmail.com>
2022-08-24 11:09:59 +0530

    new changes

--- js/function.js
+++ js/function.js
@@ -7,3 +7,50 @@ function buttontoggel(){
         pallu.classList.add('hidden')
     }
 }
+
+
+// slide part 
+let slide = document.getElementById('slide');
+let totalslides =slide.childElementCount;
+console.log(totalslides);
+let currentvalue = 1;
+
+
+
+
+
+
+function right(){
+if (currentvalue<totalslides) {
+    currentvalue++;
+    show()
+}
+};
+
+
+
+function left(){
+    if (currentvalue>1) {
+        currentvalue--;
+        show()
+    }
+    };
+
+    function show (){
+        total = document.getElementById('slide').getElementsByTagName('ul');
+        for (let index = 0; index <totalslides; index++) {
+            const element = total[index];
+            if (currentvalue === index+1) {
+                element.classList.remove('hidden')
+                
+            }else{
+                element.classList.add('hidden')
+            }
+        }
+
+    }
+setInterval(right,5000);
+
+
+
+// slide end here 
