463587573759c9463518d191982845af54c02649
kamalaak <kamalstiries007@gmail.com>
2022-08-31 08:36:29 +0530

    brand added

--- js/function.js
+++ js/function.js
@@ -92,3 +92,81 @@ function validate(){
 
 
 // contact validation part ends here 
+
+
+
+
+// slidedad
+
+let dad = document.getElementById('sliderdad');
+
+let child = document.getElementById('silderchild');
+
+let cards = child.getElementsByTagName('li');
+
+
+let dadwidth = dad.clientWidth;
+
+
+
+let cartstoshow = 3;
+
+if (document.body.clientWidth<500) {
+    cartstoshow = 1;
+}else if (document.body.clientWidth <1000){
+    cartstoshow = 3;
+}
+
+
+let cardswidth = dadwidth/cartstoshow;
+
+
+ child.style.width = cards.length*cardswidth+'px';
+ child.style.transition = "margin";
+ child.style.transitionDuration = "1s";
+
+ for (let index = 0; index < cards.length; index++) {
+    const element = cards[index];
+
+    element.style.width = cardswidth+'px';
+    
+ }
+
+function pre(){
+   
+    if (+cardswidth*(cards.length-cartstoshow) !== -child.style.marginLeft.slice(0,-2) ) {
+        child.style.marginLeft =  ((+child.style.marginLeft.slice(0,-2))-cardswidth)+'px';
+    }
+    
+}
+
+function next(){
+    if (+child.style.marginLeft.slice(0,-2) !== 0) {
+        child.style.marginLeft =  ((+child.style.marginLeft.slice(0,-2))+cardswidth)+'px';
+    }
+    
+}
+function autoplay(){
+    pre();
+    if (+cardswidth*(cards.length-cartstoshow) === -child.style.marginLeft.slice(0,-2) ){
+
+      child.style.marginLeft = "0px";
+
+    }
+    setTimeout(() => {
+        autoplay()
+    }, 5000);
+
+
+
+  
+}
+
+
+setTimeout(() => {
+    autoplay()
+}, 3000);
+
+
+
+// slide dad end here 
